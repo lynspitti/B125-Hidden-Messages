c77b97d8c53d2d1bb1fd4a137e3706aaaa0906df
Themihas <33627652+Themihas@users.noreply.github.com>
2018-05-04 08:43:39 +0200

    Daniel fixed most problems

--- Struktureret_System_Udvikling/Workshop_2/code/weatherStation/weatherStation.ino
+++ Struktureret_System_Udvikling/Workshop_2/code/weatherStation/weatherStation.ino
@@ -1,41 +1,42 @@
-class Measurement_Module {
-  virtual Get_Measurement();
-};
-
-class Temp_Measurement : public Measurement_Module {
-  virtual Get_Measurement();
-  Data_Struct Get_Measurement(){
-    int temp = random(0,40); // Random relative temp between 0 to 40 celsius
-    unsigned long TimeStamp = millis();
-    Data_Struct data (temp, 1, TimeStamp); // Type = 1 equals temp measurement
-    return data;
-  }
-};
-
-class Humidity_Measurement : public Measurement_Module {
-  virtual Get_Measurement(); 
-  Data_Struct Get_Measurement(){
-    int humid = random(25,95); // Random relative humidity between 25% to 95%
-    unsigned long TimeStamp = millis();
-    Data_Struct data (humid, 0, TimeStamp);  // Type = 0 equals humidity measurement
-    return data;
-  }
-};
-
 class Data_Struct{
-  protected
+  protected:
     int Value;
     int Type;
     unsigned long TimeStamp;
-  public
+  public:
     int GetValue(); 
     int GetType();
     int GetTime();
   Data_Struct(int Value,int Type,int TimeStamp);
 };
 
+class Measurement_Module : public Data_Struct {
+  virtual Data_Struct Get_Measurement();
+};
+
+class Temp_Measurement : public Measurement_Module {
+  public:
+    virtual Data_Struct Get_Measurement(){
+      int temp = random(0,40); // Random relative temp between 0 to 40 celsius
+      unsigned long TimeStamp = millis();
+      Data_Struct data (temp, 1, TimeStamp); // Type = 1 designates a temp measurement
+      return data;
+    }
+};
+
+class Humidity_Measurement : protected Measurement_Module {
+  virtual Data_Struct Get_Measurement(){
+    int humid = random(25,95); // Random relative humidity between 25% to 95%
+    unsigned long TimeStamp = millis();
+    Data_Struct data (humid, 0, TimeStamp);  // Type = 0 designates a humidity measurement
+    return data;
+  }
+};
+
+
+
 class Temp : public Data_Struct {
-  public
+  public:
     int GetTemp();
   Temp(int Value, int Type, int TimeStamp): Data_Struct(Value,Type,TimeStamp){
       
@@ -43,18 +44,26 @@ class Temp : public Data_Struct {
 };
 
 class Humidity : public Data_Struct {
-  public
+  public:
     int GetHumidity();
   Humidity(int Value, int Type, int TimeStamp): Data_Struct(Value,Type,TimeStamp){
     
   }
 };
 
-class Communicator {
-  void SendData(Data_Struct)(){
-    
-  }
-};
+
+void SendData(Data_Struct *Data){
+  Serial.print("#");
+  Serial.print(Data->GetValue(),DEC);
+  Serial.print("_");
+  Serial.print(Data->GetType(),DEC);
+  Serial.print("_");
+  Serial.print(Data->GetTime(),DEC);
+  Serial.print("!");
+  delay(300);
+  // Sending dataStruct contents starting and ending with special characters
+}
+
 
 void setup() {
   Serial.begin(9600);
@@ -62,4 +71,14 @@ void setup() {
 
 void loop() {
   delay(5000);
+  Temp currentTemp ();
+  Humidity currentHumidity ();
+  SendData(&currentTemp);
+  SendData(&currentHumidity);
 }
+
+
+
+
+
+
